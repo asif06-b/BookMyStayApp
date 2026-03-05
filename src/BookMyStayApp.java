4edb8bc1209451aed6ac45a9cc7acc842bd1754a
asif06-b <asifmahammad006@gmail.com>
2026-03-05 09:21:03 +0530

    UC1 Welcome Message

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,5 @@
 public class BookMyStayApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Hotel Booking Management App\nSystem initialized successfully");
+    }
 }
